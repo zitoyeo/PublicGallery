0235b71dcb5fb2182e00d02a87357452d672135f
zitoyeo <wlsgh010203@gmail.com>
2022-04-19 15:43:43 +0900

    creating image upload function with firebase

--- app/screens/MainTab.js
+++ app/screens/MainTab.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native'
+import {StyleSheet, Text, View, Image} from 'react-native'
 import { useUserContext } from '../contexts/UserContext';
+
+
 
 function MainTab () {
     const {user} = useUserContext();
     return (
         <View style={styles.block}>
-            <Text style={styles.text}>Hello,  {user.displayName}</Text>
+            {user.photoURL && (
+                <Image
+                source={{uri : user.photoURL}}
+                style={{width: 128, height : 128, marginBottom : 16}}
+                resizeMode="cover"
+                />
+            )}
+            <Text style={styles.text}>Hello, {user.displayName}</Text>
         </View>
     )
 }
